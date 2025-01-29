b693a4bdf2fcd13ca782781ca519412d8f14c269
Merus23 <mateus.satira@gmail.com>
2025-01-28 21:37:17 -0300

    fix: shadcn problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
     <div className="bg-black">
       <p className="text-white">Reset initial vite + react setup</p>
+      <Button>Click me</Button>
     </div>
   );
 }
