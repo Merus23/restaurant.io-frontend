94e10955f5489902db634fa24c05ab036785c269
Merus23 <mateus.satira@gmail.com>
2025-02-17 17:06:18 -0300

    feat: add the posibility to change the tab with the footer button

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -31,6 +31,16 @@ export default function Register({}: Props) {
   const [passwordStrength, setPasswordStrength] =
     useState<PasswordStrength>("no-content");
 
+  const [activeTab, setActiveTab] = useState("userRegisterTab");
+
+  const handleNextStep = () => {
+    if (activeTab === "userRegisterTab") {
+      setActiveTab("restaurantRegisterTab");
+    } else if (activeTab === "restaurantRegisterTab") {
+      setActiveTab("paymentTab");
+    }
+  };
+
   useEffect(() => {
     if (password.length === 0) {
       setPasswordStrength("no-content");
@@ -55,7 +65,11 @@ export default function Register({}: Props) {
 
   return (
     <div className="bg-red-200 w-full h-screen flex justify-center items-center">
-      <Tabs defaultValue="userRegisterTab" className="w-[400px]">
+      <Tabs
+        value={activeTab}
+        onValueChange={setActiveTab}
+        className="w-[400px]"
+      >
         <TabsList className="flex justify-between w-full h-14 mx-auto">
           <TabsTrigger
             id="userRegister"
@@ -66,14 +80,14 @@ export default function Register({}: Props) {
           </TabsTrigger>
           <TabsTrigger
             id="restaurantRegister"
-            value="password"
+            value="restaurantRegisterTab"
             className="h-10 w-10 rounded-full shadow-blue-300 shadow-md"
           >
             2
           </TabsTrigger>
           <TabsTrigger
             id="payment"
-            value="password"
+            value="paymentTab"
             className="h-10 w-10 rounded-full shadow-blue-300 shadow-md"
           >
             3
@@ -197,31 +211,139 @@ export default function Register({}: Props) {
               </form>
             </CardContent>
             <CardFooter className="flex justify-end">
-              <Button className="">Próximos passos</Button>
+              <Button className="" onClick={handleNextStep}>
+                Próximos passos
+              </Button>
             </CardFooter>
           </Card>
         </TabsContent>
-        <TabsContent value="password">
+        <TabsContent value="restaurantRegisterTab">
           <Card>
             <CardHeader>
-              <CardTitle>Password</CardTitle>
-              <CardDescription>
-                Change your password here. After saving, you'll be logged out.
-              </CardDescription>
+              <CardTitle>Criando seu restaurante</CardTitle>
+              <CardDescription>Insira suas informações abaixo.</CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="current">Current password</Label>
-                <Input id="current" type="password" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="new">New password</Label>
-                <Input id="new" type="password" />
-              </div>
+              <form onSubmit={() => {}}>
+                <div className="space-y-1">
+                  <Label htmlFor="fullname">Nome completo</Label>
+                  <Input
+                    id="fullname"
+                    defaultValue=""
+                    placeholder="João Mateus Silva"
+                    required
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="email">E-mail</Label>
+                  <Input
+                    id="email"
+                    placeholder="JoaoMateus@examplo.com"
+                    required
+                  />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="cpf">CPF</Label>
+                  <Input id="cpf" placeholder="123.456.789-00" required />
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="phonenumber">Número de telefone</Label>
+                  <Input
+                    id="phonenumber"
+                    placeholder="(99) 99999-9999"
+                    required
+                    type="tel"
+                  />
+                </div>
+
+                <div className="space-y-1">
+                  <label
+                    htmlFor="password"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    Senha
+                  </label>
+                  <p
+                    className={`text-xs ${
+                      passwordStrength === "Fraca"
+                        ? "text-red-500"
+                        : passwordStrength === "Média"
+                        ? "text-yellow-500"
+                        : passwordStrength === "Forte"
+                        ? "text-green-500"
+                        : "hidden"
+                    }`}
+                  >
+                    {passwordStrength !== "no-content" && passwordStrength}
+                  </p>
+                  <div className="relative">
+                    <Input
+                      id="password"
+                      type={showPassword ? "text" : "password"}
+                      placeholder="********"
+                      required
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword((prev) => !prev)}
+                      className="absolute inset-y-0 right-2 flex items-center text-gray-500"
+                    >
+                      {showPassword ? <EyeSlash /> : <Eye />}
+                    </button>
+                  </div>
+                </div>
+                <div className="space-y-1">
+                  <Label htmlFor="passwordagain">Repita sua senha</Label>
+                  <p
+                    className={`text-xs ${
+                      passwordMatch === "no-content"
+                        ? "hidden"
+                        : passwordMatch === "match"
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }`}
+                  >
+                    {passwordMatch === "match"
+                      ? "As senhas coincidem"
+                      : "As senhas não coincidem"}
+                  </p>
+                  <div className="relative">
+                    <Input
+                      id="passwordagain"
+                      type={showPassword ? "text" : "password"}
+                      placeholder="********"
+                      required
+                      value={passwordAgain}
+                      onChange={(e) => setPasswordAgain(e.target.value)}
+                    />
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword((prev) => !prev)}
+                      className="absolute inset-y-0 right-2 flex items-center text-gray-500"
+                    >
+                      {showPassword ? <EyeSlash /> : <Eye />}
+                    </button>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="hidden"
+                  id="userRegisterSubmitButton"
+                ></button>
+              </form>
             </CardContent>
-            <CardFooter>
-              <Button>Save password</Button>
+            <CardFooter className="flex justify-end">
+              <Button className="" onClick={handleNextStep}>
+                Próximos passos
+              </Button>
             </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="paymentTab">
+          <Card>
+            <h1>Qualquer coisa 13</h1>
           </Card>
         </TabsContent>
       </Tabs>
