1fb8708711645e9376680f6077b5168f45e853a6
Merus23 <mateus.satira@gmail.com>
2025-02-18 12:47:09 -0300

    refactor: redo the TabsTrigger, the every single TabsTriggers have the same properties. So a use a object to render it

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -11,9 +11,14 @@ export default function Register({}: Props) {
 
   const handleNextStep = () => {
     if (activeTab === "userRegisterTab") setActiveTab("restaurantRegisterTab");
-
     if (activeTab === "restaurantRegisterTab") setActiveTab("paymentTab");
   };
+
+  const tabProperties = [
+    { id: "userRegister", value: "userRegisterTab" },
+    { id: "restaurantRegister", value: "restaurantRegisterTab" },
+    { id: "payment", value: "paymentTab" },
+  ];
 
   return (
     <div className="bg-red-200 w-full h-screen flex justify-center pt-24 md:pt-60">
@@ -23,30 +28,19 @@ export default function Register({}: Props) {
         className="w-[400px]"
       >
         <TabsList className="flex justify-between w-full h-14 mx-auto">
-          <TabsTrigger
-            disabled
-            id="userRegister"
-            value="userRegisterTab"
-            className="h-10 w-10 rounded-full shadow-blue-300 shadow-md text-gray-900"
-          >
-            1
-          </TabsTrigger>
-          <TabsTrigger
-            disabled
-            id="restaurantRegister"
-            value="restaurantRegisterTab"
-            className="h-10 w-10 rounded-full shadow-blue-300 shadow-md text-gray-900"
-          >
-            2
-          </TabsTrigger>
-          <TabsTrigger
-            disabled
-            id="payment"
-            value="paymentTab"
-            className="h-10 w-10 rounded-full shadow-blue-300 shadow-md text-gray-900"
-          >
-            3
-          </TabsTrigger>
+          {tabProperties.map((tabProperty, index) => {
+            return (
+              <TabsTrigger
+                key={index}
+                disabled
+                id={tabProperty.id}
+                value={tabProperty.value}
+                className="h-10 w-10 rounded-full shadow-blue-300 shadow-md text-gray-900"
+              >
+                {index + 1}
+              </TabsTrigger>
+            );
+          })}
         </TabsList>
         <TabsContent value="userRegisterTab">
           <UserRegisterTabComponent handleNextStep={handleNextStep} />
