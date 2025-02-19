755a303ed144f25a59694c5273c0df09c6b000bb
Merus23 <mateus.satira@gmail.com>
2025-02-19 10:26:43 -0300

    refactor: imprive the responsivity

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -21,13 +21,13 @@ export default function Dashboard({}: Props) {
         <p>Boa tarde, João!</p>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-14 md:gap-28 p-4 md:p-0">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-14 md:gap-28">
         {cards.map((cardName, index) => {
           return (
             <a href={cardName.href} key={index} className="hover:scale-110">
-              <Card className="md:p-4 pt-4 md:pt-0">
+              <Card className="p-0 pt-4 md:p-4">
                 <CardContent className="">
-                  <div className="w-fit mx-auto ">{cardName.icon}</div>
+                  <div className="w-fit mx-auto">{cardName.icon}</div>
                 </CardContent>
                 <CardFooter className="">
                   <div className="w-fit mx-auto">
