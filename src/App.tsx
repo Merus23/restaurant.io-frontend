9a3166e1112d6234456829a2be2e8f134f28deaa
Merus23 <mateus.satira@gmail.com>
2025-02-19 13:54:27 -0300

    feat: add the logic to don't show the navbar in all pages (you can specific what page you want to show navbar)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Navbar from "./components/my/Navbar/Navbar";
 
+function Layout() {
+  let location = useLocation();
+
+  const dontShowNav = ["/register"];
+  const showNavbar = !dontShowNav.includes(location.pathname);
+
+  return (
+    <div className="w-full h-screen flex flex-col">
+      {showNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="w-full h-screen flex flex-col ">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
+      <Layout />
     </BrowserRouter>
   );
 }
